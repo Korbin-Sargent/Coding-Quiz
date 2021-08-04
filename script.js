f89f6e74bb2f6e20d954b76ea08b367549e87da0
Korbin Sargent <korbinksargent@gmail.com>
2021-08-04 13:18:39 -0600

    Adding logic for displaying next question and restarting quiz once user has answered all questions

--- script.js
+++ script.js
@@ -17,30 +17,30 @@ var questions = [
     ],
   },
   {
-    question: "what is the third planet from the sun?",
+    question: "How many planets are in our solar system, not including Pluto?",
     answers: [
-      { text: "Earth", correct: true },
-      { text: "Mars", correct: false },
-      { text: "Mercury", correct: false },
-      { text: "Venus", correct: false },
+      { text: "7", correct: false },
+      { text: "8", correct: true },
+      { text: "9", correct: false },
+      { text: "10", correct: false },
     ],
   },
   {
-    question: "what is the third planet from the sun?",
+    question: "what is the oldest planet in our solar system?",
     answers: [
-      { text: "Earth", correct: true },
-      { text: "Mars", correct: false },
+      { text: "Earth", correct: false },
+      { text: "Jupiter", correct: true },
       { text: "Mercury", correct: false },
-      { text: "Venus", correct: false },
+      { text: "Neptune", correct: false },
     ],
   },
   {
-    question: "what is the third planet from the sun?",
+    question: "what is the larget planet in our solar system?",
     answers: [
-      { text: "Earth", correct: true },
-      { text: "Mars", correct: false },
-      { text: "Mercury", correct: false },
-      { text: "Venus", correct: false },
+      { text: "Saturn", correct: false },
+      { text: "Uranus", correct: false },
+      { text: "Neptune", correct: false },
+      { text: "Jupiter", correct: true },
     ],
   },
 ];
@@ -48,23 +48,23 @@ var questions = [
 function startQuiz() {
   startButton.classList.add("hide");
   questionContainerEl.classList.remove("hide");
-  shuffledQuestions = questions.sort(function () {
-    Math.random() - 0.5;
-  });
   currentQuestionIndex = 0;
   setNextQuestion();
 }
 
 function setNextQuestion() {
   resetState();
-  currentQuestionIndex++;
-  showQuestion(shuffledQuestions[currentQuestionIndex]);
+  showQuestion(questions[currentQuestionIndex]);
+  // console.log(currentQuestionIndex);
 }
 
+//appends the question text and answer buttons to the container
 function showQuestion(question) {
   questionEl.innerText = question.question;
   question.answers.forEach(function (answer) {
+    //create buttons for each answer
     var button = document.createElement("button");
+    //insert text for each answer as the text declared in var questions
     button.innerText = answer.text;
     if (answer.correct) {
       button.dataset.correct = answer.correct;
@@ -72,10 +72,11 @@ function showQuestion(question) {
     button.addEventListener("click", selectAnswer);
     answerButtons.appendChild(button);
   });
+  // console.log(currentQuestionIndex);
 }
 
 function resetState() {
-  nextButton.classList.add("hide");
+  // nextButton.classList.add("hide");
   while (answerButtons.firstChild) {
     answerButtons.removeChild(answerButtons.firstChild);
   }
@@ -84,26 +85,37 @@ function resetState() {
 function selectAnswer(event) {
   var selectedButton = event.target;
   var correct = selectedButton.dataset.correct;
-  setStatusClass(document.body, correct);
-  Array.from(answerButtons.children).forEach(function (button) {
-    setStatusClass(button, button.dataset.correct);
-  });
-  if (shuffledQuestions.length > currentQuestionIndex + 1)
-    nextButton.classList.remove("hide");
-}
 
-function setStatusClass(element, correct) {
-  clearStatusClass(element);
-  if (correct) {
-    element.classList.add("correct");
+  console.log(selectedButton.dataset.correct);
+
+  // setStatusClass(document.body, correct);
+  Array.from(answerButtons.children).forEach(function (button) {
+    button.dataset.correct;
+  });
+  if (questions.length > currentQuestionIndex + 1) {
+    currentQuestionIndex++;
+    setNextQuestion();
+    console.log(currentQuestionIndex);
+    // nextButton.classList.remove("hide");
   } else {
-    element.classList.add("wrong");
+    startButton.classList.remove("hide");
+    startButton.innerText = "Restart";
+    startButton.addEventListener("click", startQuiz);
   }
 }
 
-function clearStatusClass(element) {
-  element.classList.remove("correct");
-  element.classList.remove("wrong");
-}
+// function setStatusClass(element, correct) {
+//   // clearStatusClass(element);
+//   if (correct) {
+//     element.classList.add("correct");
+//   } else {
+//     element.classList.add("wrong");
+//   }
+// }
+
+// function clearStatusClass(element) {
+//   element.classList.remove("correct");
+//   element.classList.remove("wrong");
+// }
 
 startButton.addEventListener("click", startQuiz);
